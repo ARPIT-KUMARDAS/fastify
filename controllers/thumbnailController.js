5deb7784b5b4149f64f66b1b222125f23c5cabf6
ARPIT-KUMARDAS <akgate2022@gmail.com>
2025-06-28 22:47:49 +0530

    first commit

---
+++ controllers/thumbnailController.js
@@ -0,0 +1,139 @@
+const Thumbnail = require("../models/thumbnail.js");
+const path = require("path");
+const fs = require("fs");
+const { pipeline } = require("stream");
+const util = require("util");
+const pipelineAsync = util.promisify(pipeline);
+
+exports.createThumbnail = async (request, reply) => {
+  try {
+    const parts = request.parts(); // Correct method for multipart
+    let fields = {};
+    let filename = null;
+
+    for await (const part of parts) {
+      if (part.file) {
+        filename = `${Date.now()}-${part.filename}`;
+        const saveTo = path.join(
+          __dirname,
+          "..",
+          "uploads",
+          "thumbnails",
+          filename
+        );
+        await pipelineAsync(part.file, fs.createWriteStream(saveTo));
+      } else {
+        fields[part.fieldname] = part.value; // Correct: use part.fieldname
+      }
+    }
+
+    if (!filename) {
+      return reply.code(400).send({ error: "No file uploaded" });
+    }
+
+    const thumbnail = new Thumbnail({
+      user: request.user.id,
+      videoName: fields.videoName,
+      version: fields.version,
+      image: `/uploads/thumbnails/${filename}`,
+      paid: fields.paid === "true",
+    });
+
+    await thumbnail.save();
+    reply.code(201).send(thumbnail);
+  } catch (err) {
+    reply.code(500).send({ error: err.message });
+  }
+};
+
+exports.getThumbnails = async (request, reply) => {
+  try {
+    const thumbnails = await Thumbnail.find({ user: request.user.id });
+    reply.send(thumbnails);
+  } catch (err) {
+    reply.code(500).send({ error: err.message });
+  }
+};
+
+exports.getThumbnail = async (request, reply) => {
+  try {
+    const thumbnail = await Thumbnail.findOne({
+      _id: request.params.id,
+      user: request.user.id,
+    });
+    if (!thumbnail) {
+      return reply.code(404).send({ error: "Thumbnail not found" });
+    }
+    reply.send(thumbnail);
+  } catch (err) {
+    reply.code(500).send({ error: err.message });
+  }
+};
+
+exports.updateThumbnail = async (request, reply) => {
+  try {
+    const updatedData = request.body;
+    const thumbnail = await Thumbnail.findOneAndUpdate(
+      { _id: request.params.id, user: request.user.id },
+      updatedData,
+      { new: true }
+    );
+    if (!thumbnail) {
+      return reply.code(404).send({ error: "Thumbnail not found" });
+    }
+    reply.send(thumbnail);
+  } catch (err) {
+    reply.code(500).send({ error: err.message });
+  }
+};
+
+exports.deleteThumbnail = async (request, reply) => {
+  try {
+    const thumbnail = await Thumbnail.findOneAndDelete({
+      _id: request.params.id,
+      user: request.user.id,
+    });
+    if (!thumbnail) {
+      return reply.code(404).send({ error: "Thumbnail not found" });
+    }
+
+    const filepath = path.join(
+      __dirname,
+      "..",
+      "uploads",
+      "thumbnails",
+      path.basename(thumbnail.image)
+    );
+    fs.unlink(filepath, (err) => {
+      if (err) request.server.log.error(err); // Use request.server.log for Fastify logging
+    });
+
+    reply.send({ message: "Thumbnail deleted" });
+  } catch (err) {
+    reply.code(500).send({ error: err.message });
+  }
+};
+
+exports.deleteAllThumbnails = async (request, reply) => {
+  try {
+    const thumbnails = await Thumbnail.find({ user: request.user.id });
+
+    await Thumbnail.deleteMany({ user: request.user.id });
+
+    for (const thumbnail of thumbnails) {
+      const filepath = path.join(
+        __dirname,
+        "..",
+        "uploads",
+        "thumbnails",
+        path.basename(thumbnail.image)
+      );
+      fs.unlink(filepath, (err) => {
+        if (err) request.server.log.error(err);
+      });
+    }
+    reply.send({ message: "All thumbnails deleted" });
+  } catch (err) {
+    reply.code(500).send({ error: err.message });
+  }
+};
